73ce416645b633069324683cd0db85fc07659360
allisonjungg <allisonjungg@gmail.com>
2018-07-12 22:41:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+var names= ["Ada","Brendan", "Ali"]
+
+function printBadges(names){
+  for (var counter = 0; counter<3; counter++) 
+  console.log("Welcome " + names[counter] + "! "+ "You are employee #" + (counter+1) + ".")
+  return names
+}
+let tails = 1
+while tails ()) {
+  
+}
