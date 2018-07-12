b5d1b5ab2b3e2b5ddc2ad0827ccee7a8f4d35190
allisonjungg <allisonjungg@gmail.com>
2018-07-12 23:07:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 // Code your solutions in this file
-var names= ["Ada","Brendan", "Ali"]
+var names= ["Ada","Brendan", "Ali"];
 
 function printBadges(names){
   for (var counter = 0; counter<3; counter++) 
-  console.log("Welcome " + names[counter] + "! "+ "You are employee #" + (counter+1) + ".")
-  return names
+  console.log("Welcome " + names[counter] + "! "+ "You are employee #" + (counter+1) + ".");
+  return names;
 }
 
-var tails = 1
-var counter = 0
 function tailsNeverFails() {
-  
+  var tails = 1;
+  var counter = 0;
   tails=Math.floor(Math.random()*2);
 }
 while (){
